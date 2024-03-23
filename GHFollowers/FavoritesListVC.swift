e6ce066b60e051d0c804596f969fac17bc0313cf
Selçuk İleri <selcukileri98@gmail.com>
2024-03-24 00:32:31 +0300

    custom buttono and custom textfield created

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Selçuk İleri on 23.03.2024.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GHFollowers/GFTextField.swift
@@ -0,0 +1,20 @@
+//
+//  GFTextField.swift
+//  GHFollowers
+//
+//  Created by Selçuk İleri on 23.03.2024.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
