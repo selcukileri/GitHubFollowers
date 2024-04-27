36087088b0c95fe0631e61e2c45846e4a75e6809
Selçuk İleri <selcukileri98@gmail.com>
2024-04-27 12:17:42 +0300

    userInfo's headerView created

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Selçuk İleri on 27.04.2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
